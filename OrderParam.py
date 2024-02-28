c6e3613df70769d7e8535741ed98d8a4d4a72e7a
sheela832 <161415750+sheela832@users.noreply.github.com>
2024-02-28 11:55:04 +0530

    Add files via upload

--- OrderParam.py
+++ OrderParam.py
@@ -3,35 +3,37 @@ def OrderParam(strategy_name,signal):
     if strategy_name =='TREND_EMA':
         # directional option selling strategy when upside movement is expected
         if signal == 1:
-            p1 = {'opt':'PE', 'step':-4,'transtype':'BUY','Qty':15, 'expiry':1}
-            p2 = {'opt':'PE', 'step':-4,'transtype':'SELL','Qty':15, 'expiry':0}
+            p1 = {'opt': 'CE', 'step': 4, 'transtype': 'BUY', 'Qty': 15, 'expiry': 1}
+            p2 = {'opt': 'CE', 'step': 4, 'transtype': 'SELL', 'Qty': 15, 'expiry': 0}
             OrderPar = {'p1':p1, 'p2':p2}
 
         elif signal == -1:
-            p1 = {'opt': 'CE', 'step': 4, 'transtype': 'BUY', 'Qty': 15, 'expiry':1}
-            p2 = {'opt': 'CE', 'step': 4, 'transtype': 'SELL', 'Qty': 15, 'expiry':0}
+            p1 = {'opt':'PE', 'step':-4,'transtype':'BUY','Qty':15, 'expiry':1}
+            p2 = {'opt':'PE', 'step':-4,'transtype':'SELL','Qty':15, 'expiry':0}
             OrderPar = {'p1':p1, 'p2':p2}
 
     elif strategy_name == 'SharpeRev':
+        # directional option selling strategy when upside movement is expected
         if signal == 1:
-            p1 = {'opt':'PE', 'step':-4,'transtype':'BUY','Qty':15, 'expiry':1}
-            p2 = {'opt':'PE', 'step':-4,'transtype':'SELL','Qty':15, 'expiry':0}
-            OrderPar = {'p1':p1, 'p2':p2}
+            p1 = {'opt': 'CE', 'step': 4, 'transtype': 'BUY', 'Qty': 15, 'expiry': 1}
+            p2 = {'opt': 'CE', 'step': 4, 'transtype': 'SELL', 'Qty': 15, 'expiry': 0}
+            OrderPar = {'p1': p1, 'p2': p2}
 
         elif signal == -1:
-            p1 = {'opt': 'CE', 'step': 4, 'transtype': 'BUY', 'Qty': 15, 'expiry':1}
-            p2 = {'opt': 'CE', 'step': 4, 'transtype': 'SELL', 'Qty': 15, 'expiry':0}
-            OrderPar = {'p1':p1, 'p2':p2}
+            p1 = {'opt': 'PE', 'step': -4, 'transtype': 'BUY', 'Qty': 15, 'expiry': 1}
+            p2 = {'opt': 'PE', 'step': -4, 'transtype': 'SELL', 'Qty': 15, 'expiry': 0}
+            OrderPar = {'p1': p1, 'p2': p2}
 
     elif strategy_name == 'MOM_BURST':
+        # directional option selling strategy when upside movement is expected
         if signal == 1:
-            p1 = {'opt':'PE', 'step':-4,'transtype':'BUY','Qty':15, 'expiry':1}
-            p2 = {'opt':'PE', 'step':-4,'transtype':'SELL','Qty':15, 'expiry':0}
-            OrderPar = {'p1':p1, 'p2':p2}
+            p1 = {'opt': 'CE', 'step': 4, 'transtype': 'BUY', 'Qty': 15, 'expiry': 1}
+            p2 = {'opt': 'CE', 'step': 4, 'transtype': 'SELL', 'Qty': 15, 'expiry': 0}
+            OrderPar = {'p1': p1, 'p2': p2}
 
         elif signal == -1:
-            p1 = {'opt': 'CE', 'step': 4, 'transtype': 'BUY', 'Qty': 15, 'expiry':1}
-            p2 = {'opt': 'CE', 'step': 4, 'transtype': 'SELL', 'Qty': 15, 'expiry':0}
-            OrderPar = {'p1':p1, 'p2':p2}
-
+            p1 = {'opt': 'PE', 'step': -4, 'transtype': 'BUY', 'Qty': 15, 'expiry': 1}
+            p2 = {'opt': 'PE', 'step': -4, 'transtype': 'SELL', 'Qty': 15, 'expiry': 0}
+            OrderPar = {'p1': p1, 'p2': p2}
+            
     return OrderPar
